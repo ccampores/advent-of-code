929d58a8a60c370e6d98e7255292c185c8d0e592
ccampores <chiara.camporesi@icloud.com>
2018-12-01 16:47:57 +0000

    1b: find first repeated frequency

--- src/1-2018.kt
+++ src/1-2018.kt
@@ -2,7 +2,25 @@ import java.io.File
 import kotlin.test.assertEquals
 
 fun main() {
-    assertEquals(516, sumFrequencies("res/1-2018-input.txt"))
+    val inputPath = "res/1-2018-input.txt"
+
+    assertEquals(516, sumFrequencies(inputPath))
+    assertEquals(71892, firstFreqTwice(inputPath))
+}
+
+fun firstFreqTwice(inputPath: String): Int {
+    val lines = File(inputPath).readLines()
+    var sum = 0
+    val freqsFound = mutableSetOf(sum)
+    do {
+        lines.forEach {
+            val el = it.toInt()
+            sum += el
+            if(!freqsFound.contains(sum))
+                freqsFound.add(sum)
+            else return sum
+        }
+    } while (true)
 }
 
 fun sumFrequencies(inputPath: String) : Int{
