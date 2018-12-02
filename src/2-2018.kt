a6e3be077b7b84b4b2e1209ba952c3da23f520de
ccampores <chiara.camporesi@icloud.com>
2018-12-02 17:03:32 +0000

    2b: find common letters in 1-char-different strings

--- src/2-2018.kt
+++ src/2-2018.kt
@@ -3,16 +3,46 @@ import kotlin.test.assertEquals
 
 fun main() {
     val inputPath = "res/2-2018-input.txt"
-    val exInputPath = "res/2-2018-exInput.txt"
+//    val exInputPath = "res/2-2018-exInput.txt"
 
-    assertEquals(12, checksum(exInputPath))
+//    assertEquals("fgij", findCommonLetters(exInputPath))
+    assertEquals("pbykrmjmizwhxlqnasfgtycdv", findCommonLetters(inputPath))
+//    assertEquals("ab", commonLettersIfOnly1Diff("abc", "abd"))
     assertEquals(6000, checksum(inputPath))
 
-    
+
+}
+
+fun findCommonLetters(inputPath: String): String? {
+    val lines = File(inputPath).readLines()
+    for(i in 0 until lines.size-1) {
+        for(j in i+1 until lines.size)
+            return commonLettersIfOnly1Diff(lines[i], lines[j]) ?: continue
+    }
+
+    return null
+}
+
+fun commonLettersIfOnly1Diff(s1: String, s2: String) : String? {
+    if(s1.length != s2.length)
+        return null
+    //assuming strings have same case as in the input file
+    var diff = 0
+    val res = StringBuilder()
+
+    for (i in 0 until s1.length) {
+        if (s1[i] != s2[i])
+            diff++
+        else res.append(s1[i])
+    }
+
+    return if (diff==1) res.toString() else null
+
 }
 
 fun checksum(inputPath: String): Int {
     var (twice, thrice) = 0 to 0
+
     File(inputPath).useLines {
         it.forEach { s ->
             val letterCounter = HashMap<Char, Int>()
@@ -28,5 +58,6 @@ fun checksum(inputPath: String): Int {
                 thrice++
         }
     }
+
     return twice * thrice
 }
