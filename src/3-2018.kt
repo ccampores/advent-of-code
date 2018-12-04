21c0e65ba811b3077b5cae288734b4d16e759072
ccampores <chiara.camporesi@icloud.com>
2018-12-04 21:01:36 +0000

    3b:find unique non overlapping

--- src/3-2018.kt
+++ src/3-2018.kt
@@ -5,33 +5,42 @@ import kotlin.test.assertEquals
 
 data class Rect(val dx:Pair<Int, Int>, val dy:Pair<Int, Int>)
 data class Point(val x:Int, val y:Int)
+data class Claim(val id:String, val area:Rect, var overlap:Boolean=false)
 
-fun findOverlapping(inputPath: String): Int {
+fun findOverlapping(inputPath: String): Pair<Int, String> {
 
     val claims = parse(inputPath)
     val overlapPoints = HashSet<Point>()
     for(i in 0 until claims.size-1) {
         for(j in i+1 until claims.size) {
-            val overlap = overlappingArea(claims[i], claims[j])
-            if (overlap != null)
+            val overlap = overlappingArea(claims[i].area, claims[j].area)
+            if (overlap != null) {
                 overlapPoints.addAll(getListOfPoints(overlap.dx, overlap.dy))
+                claims[i].overlap = true
+                claims[j].overlap = true
+            }
         }
     }
 
-    return overlapPoints.size
+    val nonOverlap = claims
+        .filter { !it.overlap }
+        .map { it.id }
+        .first()
+
+    return overlapPoints.size to nonOverlap
 }
 
-fun parse(inputPath: String): List<Rect> {
-    val regex = """#\d+ @ (\d+),(\d+): (\d+)x(\d+)""".toRegex()
-    val claims = ArrayList<Rect>()
+private fun parse(inputPath: String): List<Claim> {
+    val regex = """#(\d+) @ (\d+),(\d+): (\d+)x(\d+)""".toRegex()
+    val claims = ArrayList<Claim>()
 
     File(inputPath).useLines { sequence ->
         sequence.forEach {
             //TODO error checks
-            val (left, top, w, h) = regex.find(it)!!.destructured
+            val (id, left, top, w, h) = regex.find(it)!!.destructured
             val dx = Pair(left.toInt()+1, left.toInt()+w.toInt())
             val dy = Pair(top.toInt()+1, top.toInt()+h.toInt())
-            claims.add(Rect(dx, dy))
+            claims.add(Claim(id, Rect(dx, dy)))
         }
     }
 
@@ -72,6 +81,7 @@ fun overlappingArea(s1:Rect, s2:Rect) : Rect? {
 fun main() {
     val inputPath = "res/3-2018-input.txt"
 
-    assertEquals(107663, findOverlapping(inputPath))
+//    assertEquals(107663, findOverlapping(inputPath))
+    println(findOverlapping(inputPath))
 }
 
